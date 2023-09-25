287ed152c878322048639d89f1c3aa8fe1a87f0f
YeetRonin <aleks.traumann.004@gmail.com>
2023-09-25 12:58:58 +0300

    version2

--- js/app.js
+++ js/app.js
@@ -10,69 +10,10 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   hamburger.classList.remove("active");
   nav.classList.remove("active");
 }))
-document.addEventListener('DOMContentLoaded', function() {
-  let teenusedItems = document.querySelectorAll('.teenused-text');
-
-  teenusedItems.forEach(function(item) {
-      item.addEventListener('mouseover', function() {
-          let imageSrc = item.getAttribute('data-image');
-          let description = item.getAttribute('data-description');
-
-          if (imageSrc) {
-              document.getElementById('related-image').style.backgroundImage = 'url(images/' + imageSrc + ')';
-          }
-          if (description) {
-              document.getElementById('related-description').innerText = description;
-          }
-      });
-
-      item.addEventListener('mouseout', function() {
-          document.getElementById('related-image').style.backgroundImage = 'none';
-          document.getElementById('related-description').innerText = '';
-      });
-  });
-});
 
 
-const description = document.querySelector('.related-description');
-const image = document.querySelector('.related-image');
 
-let windowWidth = window.innerWidth;
 
-let mouseoverListener;
-
-document.addEventListener('DOMContentLoaded', function() {
-  let teenusedItems = document.querySelectorAll('.teenused-text');
-
-  teenusedItems.forEach(function(item) {
-    mouseoverListener = item.addEventListener('mouseover', function() {
-      let imageSrc = item.getAttribute('data-image');
-      let description = item.getAttribute('data-description');
-      if (imageSrc) {
-        document.getElementById('related-image').style.backgroundImage = 'url(images/' + imageSrc + ')';
-      }
-      if (description) {
-        document.getElementById('related-description').innerHTML = description;
-      }
-
-      if (windowWidth > 768) {
-        description.style.display = 'block';
-        image.style.display = 'block';
-      } else {
-        description.style.display = 'none';
-        image.style.display = 'none';
-      }
-    });
-
-    item.addEventListener('resize', function() {
-      if (windowWidth <= 768) {
-        description.style.display = 'none';
-        image.style.display = 'none';
-        item.removeEventListener('mouseover', mouseoverListener);
-      }
-    });
-  });
-});
 
 //section jumping through navbar
 
